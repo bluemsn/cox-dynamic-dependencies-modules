c05e6cb91722e6e641d22205dc7d265d8fb4568e
bluemsn <bluemsn@126.com>
2016-10-24 16:55:47 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+// 暴露一个对象 方便后续扩展
+module.exports = {
+  depModules: [{
+    defineId: 'kg/cox-slider/index',
+    dataService: {
+            // 通用描述 尽可能与mtop API中参数 格式 保持一致
+      api: '',
+      data: '',
+      param: ''
+    },
+    spmc: 'a.b.c',
+    style: {
+      topMargin: 1
+    },
+    componentData: {
+      
+    }
+  }, {
+    defineId: 'kg/cox-page-init/page/body',
+    dataService: {
+            // 通用描述 尽可能与mtop API中参数 格式 保持一致
+      api: '',
+      data: '',
+      param: ''
+    },
+    spmc: 'a.b.c',
+    componentData: {}
+  }, {
+    defineId: 'kg/cox-page-init/page/footer',
+    dataService: {
+            // 通用描述 尽可能与mtop API中参数 格式 保持一致
+      api: '',
+      data: '',
+      param: ''
+    },
+    spmc: 'a.b.c',
+    componentData: {}
+  }]
+};
